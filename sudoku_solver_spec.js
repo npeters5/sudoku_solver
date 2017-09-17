4b48fd8c2a5f34c2111cc2cc10b0e197a019f35d
Nora Peters <norapeters5@gmail.com>
2017-09-17 15:48:54 -0400

    setting stage

---
+++ sudoku_solver_spec.js
@@ -0,0 +1,8 @@
+// sudoku_solver_spec.js
+var Chai = require("chai"),
+  expect = Chai.expect,
+  solver = require("./sudoku_solver");
+
+describe("Sudoku Solver", function() {
+  // all tests should be inserted here
+});
